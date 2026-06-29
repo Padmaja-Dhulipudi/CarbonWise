41f4c29bd155ede5aa7e534139c30cb2c1295002
Padmaja Dhulipudi <163881255+Padmaja-Dhulipudi@users.noreply.github.com>
2026-06-29 20:10:26 +0530

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,10 @@
+
+server/package.json
+server/routes/authRoutes.js
+server/routes/carbonRoutes.js
+server/controllers/authController.js
+server/controllers/carbonController.js
+server/models/User.js
+server/models/CarbonHistory.js
+server/config/db.js
+server/middleware/authMiddleware.js
